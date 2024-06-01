eafb56cadbea51686d527db8cebdd3c41f74c660
JuanDuran85 <duran85juan@gmail.com>
2024-06-01 10:54:46 -0400

    bootstrap-vue and vite

---
+++ example_bv/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AlertBootstrap from "./components/AlertBootstrap.vue";
+import ModalBootstrap from "./components/ModalBootstrap.vue";
+</script>
+
+<template>
+  <AlertBootstrap />
+  <ModalBootstrap />
+</template>./components/AlertBootstrap.vue
