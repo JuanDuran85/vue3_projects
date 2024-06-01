d2bf92899bb1084e86867e4526bada0456352524
JuanDuran85 <duran85juan@gmail.com>
2024-06-01 13:17:41 -0400

    vuetify

--- example_bv/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import AlertBootstrap from "./components/AlertBootstrap.vue";
-import ModalBootstrap from "./components/ModalBootstrap.vue";
-</script>
-
-<template>
-  <AlertBootstrap />
-  <ModalBootstrap />
-</template>./components/AlertBootstrap.vue
